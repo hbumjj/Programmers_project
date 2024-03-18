ec3afe5dcbca108b58174a8cfda1c0264a90de81
hbumjj <pbj7698@gmail.com>
2024-03-19 00:39:54 +0900

    BFS 해결

--- 13460-구슬 탈출2.py
+++ 13460-구슬 탈출2.py
@@ -4,7 +4,77 @@ Created on Mon Mar 18 18:37:14 2024
 
 @author: user
 """
+from collections import deque
 
+N,M = map(int, input().split())
+
+matrix = []
+rx = 0; ry = 0; bx = 0; by = 0;
+
+for i in range(N):
+    row = list(input())
+    matrix.append(row)
+    for j in range(len(row)):
+        if row[j] == 'R':
+            rx, ry = i, j
+        if row[j] == 'B':
+            bx, by = i, j
+
+
+dx = [0, 0, 1, -1] 
+dy = [1, -1, 0, 0]
+
+que = deque()
+que.append([rx,ry,bx,by, 1])
+
+visited = [[[[False] * M for _ in range(N)] for _ in range(M)] for _ in range(N)]
+
+def move(x, y, idx): # 방향에 맞추어서 진행
+    # 위, 좌, 아래, 우
+    
+    cnt = 0
+    
+    while matrix[x+dx[idx]][y+dy[idx]] != '#' and matrix[x][y] != 'O':
+        x +=dx[idx]
+        y +=dy[idx]
+        cnt+=1
+    return x, y, cnt
+
+while que:
+    rx,ry,bx,by,depth = que.popleft()
+    go = 0
+    if depth > 10:
+        break;
+        
+    for i in range(4):
+        rrx, rry, rcnt = move(rx, ry, i)
+        bbx, bby, bcnt = move(bx, by, i)
+        
+        if matrix[bbx][bby] != 'O':
+            if matrix[rrx][rry] == 'O':
+                go = 1
+                break;
+        
+            if rrx == bbx and rry == bby: # 겹치는 부분에 대해서 제거
+                if rcnt > bcnt:
+                    rrx = rrx-dx[i]
+                    rry = rry-dy[i]
+                
+                elif rcnt < bcnt:
+                    bbx = bbx - dx[i]
+                    bby = bby - dy[i]
+            
+            if not visited[rrx][rry][bbx][bby]:
+                visited[rrx][rry][bbx][bby] = True
+                que.append([rrx,rry,bbx,bby, depth+1])
+    if go == 1:
+        print(depth)
+        break
+    
+if go == 0:
+    print(-1)
+
+'''
 N, M = 5, 5
 
 # matrix = [['#','#','#','#','#'],['#','.','.','B','#'],['#','.','#','.','#'],['#','R','O','.','#'],['#','#','#','#','#']]
@@ -95,7 +165,7 @@ else:
 
 
 
-
+'''
 
 
 
@@ -122,64 +192,64 @@ else:
 #         print()
 
 
-def up(matrix):
-    for i in range(M):
-        for j in range(N):
-            point = matrix[j][i]
+# def up(matrix):
+#     for i in range(M):
+#         for j in range(N):
+#             point = matrix[j][i]
             
-            if point == 'R':
-                 for move in range(M):
-                     if matrix[move][i] == '#' or matrix[move][i] == 'B':
-                         pass;
-                     else:
-                         matrix[move][i] = point
-                         matrix[j][i] = '.'
-                         break;
+#             if point == 'R':
+#                  for move in range(M):
+#                      if matrix[move][i] == '#' or matrix[move][i] == 'B':
+#                          pass;
+#                      else:
+#                          matrix[move][i] = point
+#                          matrix[j][i] = '.'
+#                          break;
                          
-            if point == 'B':
-                for move in range(M):
-                    if matrix[move][i] == '#' or matrix[move][i] == 'R':
-                        pass
-                    else:
-                        if move == j:
-                            pass;
-                        else:
-                            matrix[move][i] = point
-                            matrix[j][i] = '.'
-                            break;
-    return matrix
+#             if point == 'B':
+#                 for move in range(M):
+#                     if matrix[move][i] == '#' or matrix[move][i] == 'R':
+#                         pass
+#                     else:
+#                         if move == j:
+#                             pass;
+#                         else:
+#                             matrix[move][i] = point
+#                             matrix[j][i] = '.'
+#                             break;
+#     return matrix
 
-def down(matrix):
-    for i in range(M):
-        for j in range(N-1, -1, -1):
-            point = matrix[j][i]
-            if point == 'R':
-                  for move in range(M-1, -1, -1):
+# def down(matrix):
+#     for i in range(M):
+#         for j in range(N-1, -1, -1):
+#             point = matrix[j][i]
+#             if point == 'R':
+#                   for move in range(M-1, -1, -1):
 
-                      if matrix[move][i] == '#' or matrix[move][i] == 'B':
-                          pass
-                      else:
+#                       if matrix[move][i] == '#' or matrix[move][i] == 'B':
+#                           pass
+#                       else:
                           
-                          if move == j:
-                              pass
-                          else:
-                              matrix[move][i] = point
-                              matrix[j][i] = '.'
-                              break;
+#                           if move == j:
+#                               pass
+#                           else:
+#                               matrix[move][i] = point
+#                               matrix[j][i] = '.'
+#                               break;
                          
-            if point == 'B':
-                for move in range(M-1,-1,-1):
-                    if matrix[move][i] == '#' or matrix[move][i] == 'R':
-                        pass
-                    else:
-                        if move == j:
-                            pass
-                        else:
-                            matrix[move][i] = point
-                            matrix[j][i] = '.'
-                            break;
+#             if point == 'B':
+#                 for move in range(M-1,-1,-1):
+#                     if matrix[move][i] == '#' or matrix[move][i] == 'R':
+#                         pass
+#                     else:
+#                         if move == j:
+#                             pass
+#                         else:
+#                             matrix[move][i] = point
+#                             matrix[j][i] = '.'
+#                             break;
 
-    return matrix
+#     return matrix
 
 # def left(matrix):
 #     for i in range(M):
