8fdb78311e00de565fa95aa83da4508bd6b66874
Beomjun Bark <95017140+hbumjj@users.noreply.github.com>
2024-05-29 22:54:58 +0900

    정답 코드

--- 11660-구간합구하기5.py
+++ 11660-구간합구하기5.py
@@ -1,66 +1,44 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon May 27 11:44:44 2024
+Created on Wed May 29 22:53:16 2024
 
-@author: rapa
-구간합구하기 5 11660
+@author: user
 """
-from collections import deque
 
-n =  4
+import sys 
+input = sys.stdin.readline
 
+# 정답 풀이 
+N, M = map(int, input().split())
 
-matrix = [[1,2,3,4],[2,3,4,5],[3,4,5,6],[4,5,6,7]]
-
-# =============================================================================
-# 
-# =============================================================================
-
-
-# =============================================================================
-# 시간초과
-# =============================================================================
-
-def cal(start, end, matrix):
-    x = start[0]; y = start[1]
-    ex = end[0]; ey = end[1]
-    
-    q = deque()
-    q.append((x,y))
-    visited[x-1][y-1] = 1
-    sum_ = matrix[x-1][y-1]
-    
-    dx_ = [1, 0]; dy_ = [0 ,1]
-    
-    while q:
-        popq = q.popleft()
-        x = popq[0]; y= popq[1]
-        for dx, dy in zip(dx_, dy_):
-            nx = x + dx; ny = y + dy
-            if nx <= ex and ny <= ey and visited[nx-1][ny-1] == 0:
-                q.append((nx,ny))
-                visited[nx-1][ny-1] = 1
-                sum_ += matrix[nx-1][ny-1]
-            if nx == ex and ny == ey:
-                break;    
-    return sum_
-
-# =============================================================================
-# 입력
-# =============================================================================
-n, m = map(int,input().split())
 matrix = []
-
-for i in range(n):
-    a = list(map(int,input().split()))
+for i in range(N):
+    a = list(map(int, input().split()))
     matrix.append(a)
 
-for j in range(m):
-    visited = [[0] * n for _ in range(n)]
+dp = [[0] * (N+1) for _ in range (N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        dp[i][j] = dp[i][j-1] + dp[i-1][j] + matrix[i-1][j-1] - dp[i-1][j-1]
+
+
+
+
+for ss in range(M):
+    x1,y1,x2,y2 = map(int, input().split())
     
-    point = list(map(int,input().split()))
-    start = [point[0], point[1]]
-    end = [point[2], point[3]]
     
-    s = cal(start, end , matrix)
-    print(s)
+    ans = dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1]
+        
+    print(ans)        
+    
+    
+# x1,y1,x2,y2 = 2, 2 ,3, 4
+
+# print(dp[x1][y1], dp[x2][y2])
+
+# ans = dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1]
+        
+# print(ans)        
+        
