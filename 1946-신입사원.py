b4d7581a11e676058c2c7d4dbcfb5c1e65d1095a
DESKTOP-BS8LK3N\rapa <pbj7698@gmail.com>
2024-07-25 16:25:34 +0900

    그리디알고리즘 - 시간초과

---
+++ 1946-신입사원.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+
+1차, 2차
+
+B에 비해 서류 심사 결과와 면접 성적이 모두 떨어지면 A 선별 불가
+
+'''
+서류를 기준으로 정렬 후,
+criteria = 최소한의 값을 선정한다. 뽑일 때마다.
+이 criteria보다 순위가 높으면 갱신
+'''
+
+"""
+
+n = 7
+arr = [[3,6],[7,3],[4,2],[1,4],[5,7],[2,5],[6,1]]
+
+n = 5
+arr = [[3,2],[1,4],[4,1],[2,3],[5,5]]
+
+# =============================================================================
+# 
+# =============================================================================
+Tn = int(input())
+
+for test in range(Tn):
+
+    arr = []
+    number = int(input())
+    for app in range(number):
+        a = list(map(int, input().split()))
+        arr.append(a)
+
+    sarr = sorted(arr, key = lambda arr: arr[0])
+    
+    result = []
+    criteria = 0
+    for i in sarr:
+        if result == []:
+            result.append(i)
+            criteria = i[1]
+        
+        if criteria > i[1]:
+            result.append(i)
+            criteria = min(criteria, i[1])
+    
+    print(len(result))
