ec19ea07e8a7d8082691e6ab9e8457a7ffa4e549
DESKTOP-BS8LK3N\rapa <pbj7698@gmail.com>
2024-07-30 14:42:33 +0900

    그래프탐색 - 수정 예정

--- 1976-여행 가자.py
+++ 1976-여행 가자.py
@@ -1,67 +1,20 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jul 30 09:18:03 2024
-
-@author: rapa
-"""
-
-'''
-여행
-한국 도시 N
-길이 있을 수도 없을 수도
-여행 경로 가능한지 확인
-
-예
-5개 a-b b-c a-d b-d e-a
-여행 계획
-e c b c d
-
-(e) a b (c) (b) (c) b (d)
-
-입력
-도시 개수 n
-여행 계획에 속한 도시들 수 m
-
-연결 정보
-
-최종 계획 1,2,3
-'''
-
-'''
-m은 최종 변수
-'''
 n = 3
 direction = [[0,1,0],[1,0,1],[0,1,0]]
 plan = [1,2,3]
-
-'''
-1(0)으로 시작
-2(1)로 가야한다. 
-
-0 -> 열 탐색 1인 곳을 찾는다.
-append # 방문 하지 않은곳이라면,
-
-dfs? bfs? 
-'''
-
-
-n = 3
-direction = [[0,1,0],[1,0,1],[0,1,0]]
-plan = [1,2,3]
-
+m = 3
 # =============================================================================
 # 
 # =============================================================================
 
-n = int(input())
-m = int(input())
+# n = int(input())
+# m = int(input())
 
-direction = []
-for i in range(n):
-    s = list(map(int, input().split()))
-    direction.append(s)
+# direction = []
+# for i in range(n):
+#     s = list(map(int, input().split()))
+#     direction.append(s)
 
-plan = list(map(int, input().split()))
+# plan = list(map(int, input().split()))
 
 from collections import deque
 
@@ -77,7 +30,9 @@ def BFS(x, y):
         visited.append(xx) # 방문 완료        
         
         # 열 탐색
+        print(direction[xx], xx)
         for idx, t in enumerate(direction[xx]):
+            
             if t == 1 and idx not in visited: # 연결되어 있다면
                 q.append(idx)
             
@@ -86,7 +41,7 @@ def BFS(x, y):
                 break;
     return flag
 
-for si in range(1, len(plan)):
+for si in range(1, m):
     flag = BFS(plan[si-1]-1, plan[si]-1)
     if flag == False:
         break;
@@ -95,12 +50,10 @@ if flag == True:
     print("YES")
 else:
     print("NO")
-
-
-# 틀림
-
-
-
-
-
-
+    
+    
+'''
+틀린 이유
+무작정 목적지를 찾는 것이 아니라, 목적지가 아니더라도 후에 목적지에 도달할 수 있다.
+이를 고려하지 않았다.
+'''
