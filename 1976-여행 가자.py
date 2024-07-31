ed07865c36eea648e45a66fb86bdc7fee162658a
DESKTOP-BS8LK3N\rapa <pbj7698@gmail.com>
2024-07-31 13:08:02 +0900

    BFS 알고리즘

--- 1976-여행 가자.py
+++ 1976-여행 가자.py
@@ -2,19 +2,31 @@ n = 3
 direction = [[0,1,0],[1,0,1],[0,1,0]]
 plan = [1,2,3]
 m = 3
+
+
+n = 3
+direction = [[0,0,0],[0,0,0],[0,0,0]]
+plan = [1,2,3]
+m =3
+
+n = 3
+direction = [[0,1,0],[1,0,1],[0,1,0]]
+plan = [1,1]
+m = 2
 # =============================================================================
 # 
 # =============================================================================
 
-# n = int(input())
-# m = int(input())
+n = int(input())
+m = int(input())
 
-# direction = []
-# for i in range(n):
-#     s = list(map(int, input().split()))
-#     direction.append(s)
+direction = []
+for i in range(n):
+    s = list(map(int, input().split()))
+    direction.append(s)
 
-# plan = list(map(int, input().split()))
+plan = list(map(int, input().split()))
+
 
 from collections import deque
 
@@ -24,36 +36,41 @@ def BFS(x, y):
     visited = []; flag = False
     
     while q:
-        if flag == True:
-            break;
+        
         xx = q.popleft()
         visited.append(xx) # 방문 완료        
         
+        if flag == True or xx == y:
+            flag = True
+            break;
+            
         # 열 탐색
         print(direction[xx], xx)
-        for idx, t in enumerate(direction[xx]):
-            
+        for idx, t in enumerate(direction[xx]): # idx, t
+            # print(xx, idx, t, visited)   
             if t == 1 and idx not in visited: # 연결되어 있다면
                 q.append(idx)
-            
-            if idx == y: # 목적지라면
-                flag = True
-                break;
+                  
+                if idx == y: # 목적지라면
+                    flag = True
+                    break;
     return flag
 
+
 for si in range(1, m):
-    flag = BFS(plan[si-1]-1, plan[si]-1)
-    if flag == False:
+    # print(plan[si-1], plan[si])
+    
+    check = BFS(plan[si-1]-1, plan[si]-1)
+    if check == False:
         break;
 
-if flag == True:
+if check == True:
     print("YES")
 else:
     print("NO")
     
-    
+
 '''
-틀린 이유
-무작정 목적지를 찾는 것이 아니라, 목적지가 아니더라도 후에 목적지에 도달할 수 있다.
-이를 고려하지 않았다.
+<해결>
+고려 안해준 사항: 같은 여행지일 경우에는 방문할 수 있음을 간과
 '''
